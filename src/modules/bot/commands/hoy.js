0f2324856fb03b75e986affbd649bf9043fad525
llKyo <k.trivinod@gmail.com>
2025-08-19 22:18:11 -0400

    Mejoras en hoy

--- src/modules/bot/commands/hoy.js
+++ src/modules/bot/commands/hoy.js
@@ -12,16 +12,30 @@ async function obtenerTareasHoy(){
 
 }
 
+async function obtenerTareasVencidas(){
+    
+    try {
+        const data = await global.todoApi.getTasks({ filter: "overdue" })
+        return data.results
+    } catch (error) {
+        
+        return []
+    }
+    
+}
+
 function escapeMarkdownV2(texto) {
     return texto.replace(/([_*[\]()~`>#+\-=|{}.!])/g, "\\$1");
 }
 
 export async function hoy(ctx, esCron = false){
-    const tareas = await obtenerTareasHoy()
+    const tareasHoy = await obtenerTareasHoy()
+    const tareasVencidas = await obtenerTareasVencidas()
 
-    const cantidad = tareas.length
+    const cantidadHoy = tareasHoy.length
+    const cantidadVencidas = tareasVencidas.length
 
-    if (cantidad == 0) {
+    if (cantidadHoy == 0 && cantidadVencidas == 0) {
         if (!esCron) {
             await ctx.reply(`✨ No quedan tareas para el día de hoy.`)
             await ctx.reply('😄')
@@ -29,15 +43,33 @@ export async function hoy(ctx, esCron = false){
         return
     }
 
-    let mensaje = `➡️ `
-    mensaje += cantidad == 1 ? 'Hay 1 tarea pendiente\\.' : `Hay ${cantidad} tareas pendientes\\.`
-    mensaje += `\n\n`
-
+    let mensaje = ''
     let indice = 1
 
-    for (const tarea of tareas) {        
-        mensaje +=  `${indice}\\. ${escapeMarkdownV2(tarea.content)} \\- [Link 🌐](${tarea.url})\n`
-        indice++
+    if (cantidadHoy > 0) {
+        mensaje += `➡️ `
+        mensaje += cantidadHoy == 1 ? 'Hay 1 tarea para el dia de hoy\\. 😯' : `Hay ${cantidadHoy} tareas para el dia de hoy\\. 😊`
+        mensaje += `\n\n`
+
+        for (const tarea of tareasHoy) {        
+            mensaje +=  `${indice}\\. ${escapeMarkdownV2(tarea.content)} \\- [Link 🌐](${tarea.url})\n`
+            indice++
+        }
+
+        mensaje += `\n`
+    }
+
+    if (cantidadVencidas > 0) {
+        indice = 1
+
+        mensaje += `⚠️ `
+        mensaje += cantidadVencidas == 1 ? `Quedó una tarea atrasada de días anteriores\\.` : `Quedan ${cantidadVencidas} tareas atradas de días anteriores\\.`
+        mensaje += `\n\n`
+
+        for (const tarea of tareasVencidas) {        
+            mensaje +=  `${indice}\\. ${escapeMarkdownV2(tarea.content)} \\- [Link 🌐](${tarea.url})\n`
+            indice++
+        }
     }
 
     if (!esCron) {
@@ -46,7 +78,7 @@ export async function hoy(ctx, esCron = false){
         const destinatarios = await obtenerDestinatariosCron(16, esCron)
             
         destinatarios.forEach(destinatario => {             
-            global.G_bot.telegram.sendMessage(destinatario.USUARIO_ID, mensaje)
+            global.G_bot.telegram.sendMessage(destinatario.USUARIO_ID, mensaje, {parse_mode: "MarkdownV2", disable_web_page_preview: true})
         });
     }
 }
